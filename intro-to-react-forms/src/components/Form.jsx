5bbff6394eb5daf1cea32e166704ea649f1d3f1c
Wizzerrd <luis@DESKTOP-7AS3T51>
2023-07-31 16:41:13 -0700

    add form inputs

--- intro-to-react-forms/src/components/Form.jsx
+++ intro-to-react-forms/src/components/Form.jsx
@@ -1,4 +1,5 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
+
 const Form=()=>{
     const [name, setName] = useState('')
     const [email, setEmail]= useState('')
@@ -6,6 +7,16 @@ const Form=()=>{
     const [phoneType, setPhoneType] = useState('')
     const [bio, setBio] = useState('')
     const [notify, setNotify] = useState('')
+    const [validationErrors, setValidationErrors] = useState({})
+
+    useEffect(()=>{
+        const errors = {name:[], email:[]};
+        if (!name.length) errors['name'].push('Please enter your Name');
+        if (name.length > 30) errors['name'].push('Name must be less than 30 characters');
+        if (email.length && !email.includes('@')) errors['email'].push('Please provide a valid Email');
+        if (!email.length) errors['email'].push('Please provide an Email.')
+        setValidationErrors(errors);
+    }, [name, email])
 
     const onSubmit = e => {
         e.preventDefault();
@@ -24,15 +35,26 @@ const Form=()=>{
         setPhoneType('');
         setBio('');
         setNotify('');
+        setValidationErrors({});
     }
 
     return(
     <form onSubmit={onSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" type="text"  value={name} onChange={ e => setName(e.target.value)}/>
+        {validationErrors.name.length > 0 && validationErrors.name.map((error, idx) => (
+              <ul key={idx}>
+                  <li className='error'>* {error}</li>
+              </ul>
+          ))}
 
         <label htmlFor="email">Email:</label>
         <input id="email" type="text" value={email} onChange={ e => setEmail(e.target.value)} />
+        {validationErrors.email.length > 0 && validationErrors.email.map((error, idx) => (
+              <ul key={idx}>
+                  <li className='error'>* {error}</li>
+              </ul>
+          ))}
 
         <label htmlFor="number">Number:</label>
         <input id="number" type="tel" value={number} onChange={ e => setNumber(e.target.value)} />
