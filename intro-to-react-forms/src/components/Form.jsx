dd9093f9a9caf5c279cde603092f797b4e1f4481
“eliltaA” <eliltatabrham@gmail.com>
2023-07-31 17:01:03 -0700

    second commit

--- intro-to-react-forms/src/components/Form.jsx
+++ intro-to-react-forms/src/components/Form.jsx
@@ -8,18 +8,26 @@ const Form=()=>{
     const [bio, setBio] = useState('')
     const [notify, setNotify] = useState('')
     const [validationErrors, setValidationErrors] = useState({})
+    const [hasSubmitted, setHasSubmitted] = useState(false);
 
     useEffect(()=>{
-        const errors = {name:[], email:[]};
+        const errors = {name:[], email:[], bio:[]};
         if (!name.length) errors['name'].push('Please enter your Name');
         if (name.length > 30) errors['name'].push('Name must be less than 30 characters');
+
+        if (bio.length > 280) errors['bio'].push('bio too long');
+
         if (email.length && !email.includes('@')) errors['email'].push('Please provide a valid Email');
         if (!email.length) errors['email'].push('Please provide an Email.')
         setValidationErrors(errors);
-    }, [name, email])
+    }, [name, email, bio])
 
     const onSubmit = e => {
         e.preventDefault();
+
+        setHasSubmitted(true);
+        if (Object.values(validationErrors).length) return alert(`Cannot Submit`);
+    
         const inputs = {
             name, 
             email,
@@ -36,13 +44,14 @@ const Form=()=>{
         setBio('');
         setNotify('');
         setValidationErrors({});
+        setHasSubmitted(false);
     }
 
     return(
     <form onSubmit={onSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" type="text"  value={name} onChange={ e => setName(e.target.value)}/>
-        {validationErrors.name.length > 0 && validationErrors.name.map((error, idx) => (
+        {hasSubmitted && validationErrors.name.length > 0 && validationErrors.name.map((error, idx) => (
               <ul key={idx}>
                   <li className='error'>* {error}</li>
               </ul>
@@ -50,14 +59,14 @@ const Form=()=>{
 
         <label htmlFor="email">Email:</label>
         <input id="email" type="text" value={email} onChange={ e => setEmail(e.target.value)} />
-        {validationErrors.email.length > 0 && validationErrors.email.map((error, idx) => (
+        {hasSubmitted && validationErrors.email.length > 0 && validationErrors.email.map((error, idx) => (
               <ul key={idx}>
                   <li className='error'>* {error}</li>
               </ul>
           ))}
 
         <label htmlFor="number">Number:</label>
-        <input id="number" type="tel" value={number} onChange={ e => setNumber(e.target.value)} />
+        <input id="number" type="number" value={number} onChange={ e => setNumber(e.target.value)} />
 
         <label htmlFor="phoneType">PhoneType:</label>
         <select id="phoneType" value={phoneType} onChange={ e => setPhoneType(e.target.value)} >
@@ -67,6 +76,11 @@ const Form=()=>{
 
         <label htmlFor="bio">Bio:</label>
         <textarea id="bio" cols="30" rows="10" value={bio} onChange={ e => setBio(e.target.value)} placeholder="enter a bio"></textarea>
+        {hasSubmitted && validationErrors.bio.length > 0 && validationErrors.bio.map((error, idx) => (
+            <ul key={idx}>
+                <li className='error'>* {error}</li>
+            </ul>
+        ))}
 
         <label htmlFor="notify">Notify?:</label>
         <select id="notify" value={notify} onChange={ e => setNotify(e.target.value)} >
